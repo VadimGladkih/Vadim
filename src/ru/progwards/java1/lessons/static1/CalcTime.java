2a1932419cd54260433747f97596cc10292e0bf7
HP <Qwerty12>
2022-06-06 14:51:26 +0300

    Vadim

--- src/ru/progwards/java1/lessons/static1/CalcTime.java
+++ src/ru/progwards/java1/lessons/static1/CalcTime.java
@@ -58,27 +58,49 @@ class Goodies{
     }
     public static void setChild(String child) {
         {
-            child = null;
+            masha();
         }
     }
     public static void setGoodies(String goodies){
         {
-            goodies = null;
+           cookie();
         }
     }
     public static void printPresents(){
         masha();
         printPresent();
-        goodies = "леденец";
         setGoodies (goodies);
+        goodies = "леденец";
         printPresent();
         cookie();
         printPresent();
-        child = "Пете";
         setChild(child);
+        child = "Пете";
         printPresent();
     }
     public static void main(String[]args){
         printPresents();
+    }
+}
+class Room{
+    static double length1 = 5d;
+    static  double width1 = 3d;
+    static double length2 = length1 + 1.5d;
+    static double width2 = width1 * 1.5d;
+    static double square1 = square(length1,width1);
+    static double square2 = square(length2,width2);
+    public static double square(double length, double width){
+        double result = length * width;
+        return result;
+    }
+    public static void printRoom(String name, double square){
+        System.out.println("Площадь " + name + " составляет " + square + " м2");
+    }
+    public static void printFlat(){
+        printRoom("кухни",square1);
+        printRoom("гостинной",square2);
+    }
+    public static void main(String[]args){
+        printFlat();
     }
 }

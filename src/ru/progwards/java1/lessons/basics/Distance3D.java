2002dfb41250878e43e37c50de2b7e84f78ea673
HP <Qwerty12>
2022-06-09 14:44:10 +0300

    Vadim

--- src/ru/progwards/java1/lessons/basics/Distance3D.java
+++ src/ru/progwards/java1/lessons/basics/Distance3D.java
@@ -9,13 +9,10 @@ public class Distance3D {
     static double z2;
 
     public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
-        double a = x1 - y1;
-        double b = z1 - x2;
-        double c = y2 - z2;
-        return Math.sqrt(a*a+b*b+c*c);
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
     }
 
     public static void main(String[] args) {
-        System.out.println("AB = " + distance(x1, y1, z1, x2, y2, z2));
+        System.out.println("AB = " + distance(x1,y1,z1,x2,y2,z2));
     }
 }

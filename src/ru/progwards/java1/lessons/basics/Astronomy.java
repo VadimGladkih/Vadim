8645471a9eb24a56ba4b63fedc25d72939c365eb
HP <Qwerty12>
2022-06-17 21:57:51 +0300

    Vadim

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -2,23 +2,40 @@ package ru.progwards.java1.lessons.basics;
 
 public class Astronomy {
     static final double pi = 3.14d;
-
-    public static double sphereSquare(double r) {
-        System.out.println("S = " + 4 * pi * Math.pow(r,2));
-        return r;
+    static final double ERATH_RADIUS = 6371.2;
+    static final double MERCURY_RADIUS = 2439.7;
+    static final double JUPITER_RADIUS = 71492.0;
+    public static Double sphereSquare(Double r) {
+        return Double.valueOf(4 * pi * Math.pow(r,2));
     }
-
-    public static Double earthSquare() {
-        System.out.println(sphereSquare(6371.2));
+    public static Double earthSquare(){
+        System.out.println(sphereSquare(ERATH_RADIUS));
         return null;
     }
     public static Double mercurySquare(){
-        System.out.println(sphereSquare(2439.7));
+        System.out.println(sphereSquare(MERCURY_RADIUS));
+        return null;
+    }
+    public static Double jupiterSquare(){
+        System.out.println(sphereSquare(JUPITER_RADIUS));
+        return null;
+    }
+    public static Double earthVsMercury(){
+        System.out.println(sphereSquare(ERATH_RADIUS)/sphereSquare(MERCURY_RADIUS));
+        return null;
+    }
+    public static Double earthVsJupiter(){
+        System.out.println(sphereSquare(ERATH_RADIUS)/sphereSquare(JUPITER_RADIUS));
         return null;
     }
 
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         earthSquare();
         mercurySquare();
+        jupiterSquare();
+        earthVsMercury();
+        earthVsJupiter();
+
     }
 }

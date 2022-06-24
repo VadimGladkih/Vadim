c14cafa7bfaa5e3b98a5a4f9e9712231bda55531
HP <Qwerty12>
2022-06-24 12:42:46 +0300

    Vadim

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -5,23 +5,37 @@ public class Astronomy {
     static final double ERATH_RADIUS = 6371.2;
     static final double MERCURY_RADIUS = 2439.7;
     static final double JUPITER_RADIUS = 71492.0;
+
     public static Double sphereSquare(Double r) {
-        return Double.valueOf(4 * pi * Math.pow(r,2));
+        return Double.valueOf(4 * pi * Math.pow(r, 2));
     }
-    public static Double earthSquare(){
+
+    public static Double earthSquare() {
         return sphereSquare(ERATH_RADIUS);
     }
-    public static Double mercurySquare(){
+
+    public static Double mercurySquare() {
         return sphereSquare(MERCURY_RADIUS);
     }
-    public static Double jupiterSquare(){
+
+    public static Double jupiterSquare() {
         return sphereSquare(JUPITER_RADIUS);
     }
-    public static Double earthVsMercury(){
-        return sphereSquare(ERATH_RADIUS)/sphereSquare(MERCURY_RADIUS);
+
+    public static Double earthVsMercury() {
+        return sphereSquare(ERATH_RADIUS) / sphereSquare(MERCURY_RADIUS);
     }
-    public static Double earthVsJupiter(){
-        return sphereSquare(ERATH_RADIUS)/sphereSquare(JUPITER_RADIUS);
+
+    public static Double earthVsJupiter() {
+        return sphereSquare(ERATH_RADIUS) / sphereSquare(JUPITER_RADIUS);
+    }
+
+    static int addAsStrings(int n1, int n2) {
+        String str1 = Integer.toString(n1);
+        String str2 = Integer.toString(n2);
+        String str3 = str1 + str2;
+        Integer n3 = Integer.valueOf(str3);
+        return n3;
     }
 
 
@@ -31,6 +45,8 @@ public class Astronomy {
         jupiterSquare();
         earthVsMercury();
         earthVsJupiter();
-
+        short i = (short) 256567.789;
+        System.out.println(i);
+        System.out.println(addAsStrings(1, 2));
     }
 }

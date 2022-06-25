a5a7b2f3254f9ba1d2d0949e31982d07273a0514
HP <Qwerty12>
2022-06-25 16:50:58 +0300

    Vadim

--- src/ru/progwards/java1/lessons/custing/Figures.java
+++ src/ru/progwards/java1/lessons/custing/Figures.java
@@ -8,14 +8,26 @@ public class Figures {
     public static double square(double n){
         return n*n;
     }
-    public static Double triangle(double k){
-        double p = (k+k+k)/2;
-        return Math.sqrt(p*(p-k)*(p-k)*(p-k));
+    public static Double triangle(double k) {
+        Double p = (k + k + k) / 2;
+        return Math.sqrt(p * (p - k) * (p - k) * (p - k));
+    }
+    public static double squareVsTraiange(double p){
+        return square(p)/triangle(p);
+    }
+    public static double squareVsCircle(double p){
+        return square(p)/circle(p);
+    }
+    public static double triangleVsCircle(double p){
+        return triangle(p)/circle(p);
     }
 
     public static void main(String[] args) {
         System.out.println(circle(10));
         System.out.println(square(10));
         System.out.println(triangle(12));
+        System.out.println(squareVsTraiange(21));
+        System.out.println(squareVsCircle(32));
+        System.out.println(triangleVsCircle(32));
     }
 }

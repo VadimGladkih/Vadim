54bd24dfa16903090af49b93a409a4310c6acde5
HP <Qwerty12>
2022-06-27 22:11:54 +0300

    Vadim

---
+++ src/ru/progwards/java1/lessons/compare/TriangleSimpleInfo.java
@@ -0,0 +1,35 @@
+package ru.progwards.java1.lessons.compare;
+
+public class TriangleSimpleInfo {
+    public static int maxSide(int a, int b, int c){
+        if (a>b&&a>c){
+            return a;
+        }else if (b>a&&b>c){
+            return b;
+        }else {
+            return c;
+        }
+    }
+    public static int minSide(int a, int b, int c){
+        if (a<b&&a<c){
+            return a;
+        } else if (b<a&&b<c) {
+            return b;
+        }else {
+            return c;
+        }
+    }
+    public static boolean isEquilateralTriangle(int a, int b, int c){
+        if (a==b&&b==c&&a==c){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSide(3,5,6));
+        System.out.println(minSide(6,2,7));
+        System.out.println(isEquilateralTriangle(2,2,3));
+    }
+}

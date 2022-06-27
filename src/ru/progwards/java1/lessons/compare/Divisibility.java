88724d24a2309790d753a2ac7af1eafb4706c7b9
HP <Qwerty12>
2022-06-27 11:03:05 +0300

    Vadim

--- src/ru/progwards/java1/lessons/compare/Divisibility.java
+++ src/ru/progwards/java1/lessons/compare/Divisibility.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.compare;
 
 public class Divisibility {
     public static void check(int num1, int num2) {
-        String res = "Частное от деления" + num1 + " на " + num2 + " равно ";
+        String res = "Частное от деления " + num1 + " на " + num2 + " равно ";
         if (num2 == 0) {
             System.out.println("На ноль делить нельзя");
         } else {

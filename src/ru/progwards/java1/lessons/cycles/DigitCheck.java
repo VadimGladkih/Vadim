9d8818bb5240d52450e8e90209a4e151667985b7
HP <Qwerty12>
2022-07-02 15:00:10 +0300

    Vadim

--- src/ru/progwards/java1/lessons/cycles/DigitCheck.java
+++ src/ru/progwards/java1/lessons/cycles/DigitCheck.java
@@ -2,13 +2,13 @@ package ru.progwards.java1.lessons.cycles;
 
 public class DigitCheck {
     public static boolean containsDigit(int number, int digit) {
-        for (digit = 0; digit < number; digit++) {
-            System.out.println(true);
-        }
-        return true;
+        String numberToStr = Integer.toString(number);
+        String digitToStr = Integer.toString(digit);
+        return numberToStr.contains(digitToStr);
+
     }
 
     public static void main(String[] args) {
-        containsDigit(123, 2);
+        System.out.println(containsDigit(80, 0));
     }
 }

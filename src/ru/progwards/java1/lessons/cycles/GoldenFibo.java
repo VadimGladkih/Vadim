4fb7b0fc59bf2130a40642eebaea355b21f5555d
HP <Qwerty12>
2022-07-03 15:53:45 +0300

    Vadim

--- src/ru/progwards/java1/lessons/cycles/GoldenFibo.java
+++ src/ru/progwards/java1/lessons/cycles/GoldenFibo.java
@@ -14,7 +14,7 @@ public class GoldenFibo {
     }
 
     public static boolean isGoldenTriangle(int a, int b, int c) {
-        if (a == b || b == c || a == c) {
+        if (a == b && b == c && a == c) {
             return true;
         } else if (a / b >= 1.61703 && a / b <= 1.61903 && b / c >= 1.61703 && b / c <= 1.61903 && a / c >= 1.61703 && a / c <= 1.61903) {
             return true;
@@ -26,6 +26,6 @@ public class GoldenFibo {
 
     public static void main(String[] args) {
         System.out.println(fiboNumber(10));
-        System.out.println(isGoldenTriangle(55, 34, 34));
+        System.out.println(isGoldenTriangle(34, 34, 55));
     }
 }

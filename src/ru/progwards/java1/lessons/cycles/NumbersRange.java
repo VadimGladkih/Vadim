12c668ff33ec573d69934f588648eca2ee9c500e
HP <Qwerty12>
2022-07-03 09:23:31 +0300

    Vadim

--- src/ru/progwards/java1/lessons/cycles/NumbersRange.java
+++ src/ru/progwards/java1/lessons/cycles/NumbersRange.java
@@ -2,28 +2,39 @@ package ru.progwards.java1.lessons.cycles;
 
 public class NumbersRange {
     public static long sumNumbers(int start, int finish) {
-        long sum =0;
+        long sum = 0;
         int i = start;
-        for (i=start;i<=finish;i++){
-            sum+=i;
+        for (i = start; i <= finish; i++) {
+            sum += i;
         }
         return sum;
     }
-    public static long sumOdd(int start, int finish){
+
+    public static long sumOdd(int start, int finish) {
         long sum = 0;
         int i = start;
-        for (i=start;i<=finish;i++){
-            if (i%2==0) {
+        for (i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
                 sum += i;
             }
         }
         return sum;
     }
 
+    public static long sumEvenIdx(int start, int finish) {
+        long sum = 0;
+        int i = start;
+        for (i = start; i <= finish; i++) {
+            sum += i++;
+        }
+    return sum;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(sumNumbers(11,0));
-        System.out.println(sumOdd(11,18));
+        System.out.println(sumNumbers(11, 18));
+        System.out.println(sumOdd(11, 18));
+        System.out.println(sumEvenIdx(11,18));
     }
 
 

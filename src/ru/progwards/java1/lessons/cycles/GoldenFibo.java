3ed1b14d900553ffead9e1a1dbee36cb23444dc9
HP <Qwerty12>
2022-07-03 15:21:25 +0300

    Vadim

--- src/ru/progwards/java1/lessons/cycles/GoldenFibo.java
+++ src/ru/progwards/java1/lessons/cycles/GoldenFibo.java
@@ -2,10 +2,10 @@ package ru.progwards.java1.lessons.cycles;
 
 public class GoldenFibo {
     public static int fiboNumber(int n) {
-        int sum =0;
+        int sum =1;
         int n1=1;
         int n2=1;
-        for (int i =3;i<=n;i++){
+        for (int i =3;i<=n;++i){
             sum=n1+n2;
             n1=n2;
             n2=sum;
@@ -13,7 +13,7 @@ public class GoldenFibo {
         return sum;
     }
     public static boolean isGoldenTriangle(int a, int b, int c){
-        if (a==b&&b==c&&c==a){
+        if (a==b||b==c||c==a){
         } if (a/b>=1.61703&&a/b<=1.61903&&b/c>=1.61703&&b/c<=1.61903&&a/c>=1.61703&&a/c<=1.61903) {
             System.out.println(true);
         }else {
@@ -24,6 +24,6 @@ public class GoldenFibo {
 
     public static void main(String[] args) {
         System.out.println(fiboNumber(10));
-        System.out.println(isGoldenTriangle(12,13,12));
+        System.out.println(isGoldenTriangle(34,34,55));
     }
 }

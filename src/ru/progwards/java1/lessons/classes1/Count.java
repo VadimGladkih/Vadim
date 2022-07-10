5401ec9736a54d3143041d2621376e4cf34da19b
HP <Qwerty12>
2022-07-10 10:11:16 +0300

    Vadim

---
+++ src/ru/progwards/java1/lessons/classes1/Count.java
@@ -0,0 +1,28 @@
+package ru.progwards.java1.lessons.classes1;
+
+public class Count {
+    int count;
+
+    public Count(int count) {
+        this.count = count;
+    }
+    public int getCount(){
+        System.out.println(count);
+        return count;
+    }
+    public void inc(){
+        System.out.println(count++);
+    }
+    public boolean dec(){
+        System.out.println(count--);
+        if (count<=0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Count Count1 = new Count(10);
+    }
+}

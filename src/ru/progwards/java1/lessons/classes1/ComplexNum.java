bae52ea0b91a0e9871fb6abf109be8f9f144aaee
HP <Qwerty12>
2022-09-16 14:12:14 +0300

    Vadim

--- src/ru/progwards/java1/lessons/classes1/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes1/ComplexNum.java
@@ -26,7 +26,7 @@ public class ComplexNum {
         return new ComplexNum((this.a*num.a)-(this.b*num.b), (this.b*num.a)+(this.a*num.b));
     }
     public ComplexNum div(ComplexNum num){
-        return new ComplexNum((this.a*num.a+this.b*num.b)/(num.a*num.a+num.b/num.b),(this.b*num.a-this.a*num.b)/(num.a*num.a+num.b*num.b));
+        return new ComplexNum((this.a*num.a+this.b*num.b)/(num.a*num.a+num.b*num.b),(this.b*num.a-this.a*num.b)/(num.a*num.a+num.b*num.b));
     }
 
     public static void main(String[] args) {

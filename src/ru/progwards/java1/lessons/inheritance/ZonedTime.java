976e7ef166a4ad6ab143d1789cf9e2850b163668
HP <Qwerty12>
2022-12-30 21:20:57 +0300

    Vadim

--- src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
+++ src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
@@ -1,6 +1,6 @@
 package ru.progwards.java1.lessons.inheritance;
 
-public class ZonedTime extends Time{
+public class ZonedTime extends Time {
     TimeZone zone;
 
     public ZonedTime(int hours, int minutes, int seconds) {
@@ -9,29 +9,32 @@ public class ZonedTime extends Time{
         this.minutes = minutes;
         this.seconds = seconds;
     }
-    public ZonedTime(int hours, int minutes, int seconds, TimeZone zone){
+
+    public ZonedTime(int hours, int minutes, int seconds, TimeZone zone) {
         super(hours, minutes, seconds);
         this.zone = zone;
     }
-    @Override
-    public TimeZone getTimeZone(){
+
+    //@Override
+    public TimeZone getTimeZone() {
         return zone;
     }
+
     @Override
-    public int secondsBetween(Time time){
+    public int secondsBetween(Time time) {
         TimeZone k1 = this.getTimeZone();
         TimeZone k2 = this.getTimeZone();
-        int sdvigk1,sdvigk2;
+        int sdvigk1, sdvigk2;
 
-        if (k1==null)
-            sdvigk1 =0;
+        if (k1 == null)
+            sdvigk1 = 0;
         else
-            sdvigk1 = Math.abs(k1.hours*60*60+k1.minutes*60);
-        if(k2==null)
-            sdvigk2=0;
+            sdvigk1 = Math.abs(k1.hours * 60 * 60 + k1.minutes * 60);
+        if (k2 == null)
+            sdvigk2 = 0;
         else
-            sdvigk2 = Math.abs(k2.hours * 60*60+k2.minutes*60);
-        return super.secondsBetween(time)+Math.abs(sdvigk1-sdvigk2);
+            sdvigk2 = Math.abs(k2.hours * 60 * 60 + k2.minutes * 60);
+        return super.secondsBetween(time) + Math.abs(sdvigk1 - sdvigk2);
     }
 
     public static void main(String[] args) {

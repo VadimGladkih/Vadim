79634386214e292b7db9fdb3f569ef5f005c0f61
HP <Qwerty12>
2023-01-09 17:03:35 +0300

    Vadim

--- src/ru/progwards/java1/lessons/classescompare/Animal.java
+++ src/ru/progwards/java1/lessons/classescompare/Animal.java
@@ -1,6 +1,6 @@
 package ru.progwards.java1.lessons.classescompare;
 
-public abstract class Animal {
+/*public abstract class Animal {
     String name;
     Double weight;
 
@@ -52,6 +52,68 @@ public abstract class Animal {
        }
     }
 
+    public static void main(String[] args) {
+
+    }
+}*/
+public abstract class Animal {
+    String name;
+    double weight;
+    enum FoodKind{
+        HAY,
+        CORN,
+    }
+    public Animal(String name){
+        this.name = name;
+    }
+
+    public Animal() {
+        say().toString();
+    }
+
+    public abstract String kind();
+    public abstract String say();
+    public String toString(){
+        return "Это " + kind() + " " + name;
+    }
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public double getWeight(){
+        return weight;
+    }
+    int compareTo(Animal animal){
+        if(this.weight == animal.weight) {
+            return 0;
+        }
+        if(this.weight > animal.weight){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+    public boolean equals(Animal o){
+
+        return  (this.name.equals(o.name))
+                && (this.kind().equals(o.kind()))
+                && (this.weight == o.weight);
+    }
+    abstract public FoodKind getFoodKind();
+    abstract public double getFoodCoeff();
+    public double calculateFoodWeight(){
+        return weight * getFoodCoeff();
+    }
+    public double calculateFoodPrice(){
+        int price;
+        if(getFoodKind() == FoodKind.HAY){
+            price = 2;
+        }
+        else {
+            price = 15;
+        }
+        return calculateFoodWeight() * price;
+    }
     public static void main(String[] args) {
 
     }

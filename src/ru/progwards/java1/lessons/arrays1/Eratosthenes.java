cfed601f898f8069c908c0747ee0a8febd62ad8a
HP <Qwerty12>
2023-01-13 21:59:16 +0300

    Vadim

---
+++ src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
@@ -0,0 +1,37 @@
+package ru.progwards.java1.lessons.arrays1;
+
+public class Eratosthenes {
+    private boolean[] sieve;
+    int N;
+    public Eratosthenes(int N){
+        this.N = N;
+    }
+    private void sift(){
+        for(int i=2; i * i < N; i++){
+            boolean sieve = true;
+            for (int j = 2; j*j<N;j++){
+                if(i%j==0){
+                    sieve = false;
+                    break;
+                }
+            }
+            if(sieve);
+            System.out.println(i);
+        }
+
+    }
+    public boolean isSimple(int n){
+        if (n<2)
+            return false;
+        double s = Math.sqrt(n);
+        for (int i = 2; i<=s; i++){
+            if (n%i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

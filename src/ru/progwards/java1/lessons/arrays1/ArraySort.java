535bc67e7eaea648fed8a181832168125038d830
HP <Qwerty12>
2023-01-17 10:31:07 +0300

    Vadim

--- src/ru/progwards/java1/lessons/arrays1/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays1/ArraySort.java
@@ -4,9 +4,20 @@ import java.util.Arrays;
 
 public class ArraySort {
     public static void sort(int[] a){
-        int[] sort1 = new int[8];
-        Arrays.sort(sort1);
-        System.out.println(sort1);
+        int stepsCount = a.length -1;
+        boolean swapped;
+        do{
+            swapped = false;
+            for (int i = 0; i < stepsCount; i++){
+                if(a[i]>a[i+1]){
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            stepsCount--;
+        }while (swapped);
     }
 
     public static void main(String[] args) {

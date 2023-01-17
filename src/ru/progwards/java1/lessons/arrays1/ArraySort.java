972aeb95c70e52874adf8eb4c0e2d7dad8d0f56f
HP <Qwerty12>
2023-01-17 10:18:28 +0300

    Vadim

---
+++ src/ru/progwards/java1/lessons/arrays1/ArraySort.java
@@ -0,0 +1,16 @@
+package ru.progwards.java1.lessons.arrays1;
+
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void sort(int[] a){
+        int[] sort1 = new int[8];
+        Arrays.sort(sort1);
+        System.out.println(sort1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
--- src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
@@ -3,29 +3,32 @@ package ru.progwards.java1.lessons.arrays1;
 public class Eratosthenes {
     private boolean[] sieve;
     int N;
-    public Eratosthenes(int N){
+
+    public Eratosthenes(int N) {
         this.N = N;
     }
-    private void sift(){
-        for(int i=2; i * i < N; i++){
+
+    private void sift() {
+        for (int i = 2; i * i < N; i++) {
             boolean sieve = true;
-            for (int j = 2; j*j<N;j++){
-                if(i%j==0){
+            for (int j = 2; j * j < N; j++) {
+                if (i % j == 0) {
                     sieve = false;
                     break;
                 }
             }
-            if(sieve);
+            if (sieve) ;
             System.out.println(i);
         }
 
     }
-    public boolean isSimple(int n){
-        if (n<2)
+
+    public boolean isSimple(int n) {
+        if (n < 2)
             return false;
         double s = Math.sqrt(n);
-        for (int i = 2; i<=s; i++){
-            if (n%i == 0)
+        for (int i = 2; i <= s; i++) {
+            if (n % i == 0)
                 return false;
         }
         return true;

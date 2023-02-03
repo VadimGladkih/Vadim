10a9df2af45dd77b0068c3d862679cd23c65c7c0
HP <Qwerty12>
2023-02-03 19:36:43 +0300

    Vadim

---
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -0,0 +1,14 @@
+package ru.progwards.java1.lessons.arrays2;
+
+public class IntArrays {
+    public static String toString(int []a){
+        for (int i : a){
+            System.out.println(i);
+        }
+        return String.valueOf(a);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

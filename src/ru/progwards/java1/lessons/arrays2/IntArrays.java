fdcb548dddd1f0a59b1151ab99e2034afa5f40f8
HP <Qwerty12>
2023-02-09 15:52:26 +0300

    Vadim

--- src/ru/progwards/java1/lessons/arrays2/IntArrays.java
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -3,29 +3,35 @@ package ru.progwards.java1.lessons.arrays2;
 import java.util.Arrays;
 
 public class IntArrays {
-    public static String toString(int []a){
+    public static String toString(int[] a) {
         String b = "[";
-        for (int i = 0; i< a.length; i++){
-            if (i< a.length -1){
+        for (int i = 0; i < a.length; i++) {
+            if (i < a.length - 1) {
                 b = b + a[i] + "," + " ";
             }
-            if (i == a.length -1){
+            if (i == a.length - 1) {
                 b += a[i] + "]";
             }
         }
         return b;
     }
 
-    public static boolean equals1(int[] a1, int[] a2){
-        if (a1 == a2){
+    public static boolean equals1(int[] a1, int[] a2) {
+        if (a1 == a2) {
             return true;
-        }else{
-            return false;
         }
+        for (int i =0; i< a1.length; i++) {
+            if (a1[i] != a2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
-    public static boolean equals2(int[] a1, int[] a2){
+
+    public static boolean equals2(int[] a1, int[] a2) {
         return Arrays.equals(a1, a2);
     }
+
     public static void main(String[] args) {
 
     }
